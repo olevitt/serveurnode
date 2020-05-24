eac7da0c90ec0a59f3ae42a6026cd37503ca9c9b
Olivier Levitt <ungawa14@gmail.com>
2020-05-24 21:04:15 +0200

    Serveur un peu plus intelligent

--- main.js
+++ main.js
@@ -2,7 +2,12 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => res.send("Hello World! 2"));
+app.get("/toto", (req, res) =>
+  res.send({
+    pseudo: "toto",
+  })
+);
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
